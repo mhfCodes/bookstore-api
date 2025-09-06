6314c82cf2273b7442e9b872f657b053ec9468ba
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2025-09-06 09:41:19 +0330

    initialized project

---
+++ src/test/java/com/mhf/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhf.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
