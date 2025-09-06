903209d08bdf757e826c325a6199d6955982bbd4
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2025-09-06 10:35:43 +0330

    added mapstruct, added BookDto

---
+++ src/main/java/com/mhf/bookstore/dto/book/BookDto.java
@@ -0,0 +1,66 @@
+package com.mhf.bookstore.dto.book;
+
+import com.mhf.bookstore.model.book.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class BookDto {
+
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than 0")
+    private Double price;
+
+    private Status status;
+
+    public BookDto() {
+    }
+
+    public BookDto(Long id, String title, String author, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+}
