e69fff2618740b4ca2aca29e6a2bacaa662f662c
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2025-09-06 10:43:44 +0330

    added IBookRepository

---
+++ src/main/java/com/mhf/bookstore/dao/book/IBookRepository.java
@@ -0,0 +1,10 @@
+package com.mhf.bookstore.dao.book;
+
+import com.mhf.bookstore.model.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBookRepository extends JpaRepository<Book, Long> {
+    
+}
